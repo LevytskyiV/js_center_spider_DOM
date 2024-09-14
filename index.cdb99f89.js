cca695d9df6ad1fcafc123a22631a802826b0326
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-14 16:48:12 +0300

    make build

---
+++ index.cdb99f89.js
@@ -0,0 +1,2 @@
+window.onload=function(){var e=document.querySelector(".spider"),t=document.querySelector(".wall");function n(){var n=t.clientWidth,i=t.clientHeight,o=e.clientWidth,c=e.clientHeight;e.style.position="absolute",e.style.top="".concat((i-c)/2,"px"),e.style.left="".concat((n-o)/2,"px")}n(),window.addEventListener("resize",n)};
+//# sourceMappingURL=index.cdb99f89.js.map
